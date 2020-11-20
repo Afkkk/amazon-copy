3c679890e1c06087c0f5eb7a47be4e1782db1480
AFKKK <kklaptocz1617@gmail.com>
2020-11-20 17:54:59 +0100

    commit_3

--- src/reducer.js
+++ src/reducer.js
@@ -9,8 +9,12 @@ export const initialState = {
     basket?.reduce((amount, item) => item.price + amount, 0);
   
   const reducer = (state, action) => {
-    console.log(action);
     switch (action.type) {
+      case "ADD_TO_BASKET":
+        return{
+          ...state,
+          basket: [...state.basket, action.item]
+        }
       default:
         return state;
     }
