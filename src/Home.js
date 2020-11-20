2d2428302bbcb0cc122d1e4de5c36330faf7d464
AFKKK <kklaptocz1617@gmail.com>
2020-11-20 17:31:44 +0100

    commit__3

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,8 @@ function Home() {
                     alt="amazon baner"
                 />
                <div className="home__row">
-                    <Product 
+                    <Product
+                    id="31232241" 
                     title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback" 
                     price={29.99} 
                     image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" 
