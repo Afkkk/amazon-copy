dd1607c0f1cd20e8ac07ce0356cb7721cb678f66
Afkkk <47856497+Afkkk@users.noreply.github.com>
2020-11-20 14:54:25 +0100

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDr6b6kaGnYyG6myd04kpzAyutF4GWEFA8",
+    authDomain: "clone-by-affk.firebaseapp.com",
+    databaseURL: "https://clone-by-affk.firebaseio.com",
+    projectId: "clone-by-affk",
+    storageBucket: "clone-by-affk.appspot.com",
+    messagingSenderId: "905532903769",
+    appId: "1:905532903769:web:c8b6e60a59c75b1e0cbc48",
+    measurementId: "G-ZCB57YPPN2"
+  };
