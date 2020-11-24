efb3909e3e410f50945b7c987e3dbe8f7006abb0
AFKKK <kklaptocz1617@gmail.com>
2020-11-24 11:29:48 +0100

    commit_9
    added inputs to login

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Login.css"
 
 function Login() {
     return (
         <div className="login">
-            <img 
+            <Link to="/">
+            <img
+            className="login__logo" 
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/150px-Amazon_logo.svg.png" 
             alt=""/>
-            
+            </Link>
+            <div className="login__container">
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text" />
+                    <h5>Password</h5>
+                    <input type="password" />
+                </form>
+            </div>
         </div>
     )
 }
