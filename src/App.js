10423740fc7668d69946885fffc2b9fe0c5fdccb
AFKKK <kklaptocz1617@gmail.com>
2020-11-24 14:30:30 +0100

    commit_12
    now you can create your account and sing-in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import userEvent from "@testing-library/user-event";
 
 function App() {
+
+  useEffect(()=>{
+
+    
+
+  },[])
+
   return (
     <Router>
     <div className="App">
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDr6b6kaGnYyG6myd04kpzAyutF4GWEFA8",
-    authDomain: "clone-by-affk.firebaseapp.com",
-    databaseURL: "https://clone-by-affk.firebaseio.com",
-    projectId: "clone-by-affk",
-    storageBucket: "clone-by-affk.appspot.com",
-    messagingSenderId: "905532903769",
-    appId: "1:905532903769:web:c8b6e60a59c75b1e0cbc48",
-    measurementId: "G-ZCB57YPPN2"
-  };
+  apiKey: "AIzaSyDr6b6kaGnYyG6myd04kpzAyutF4GWEFA8",
+  authDomain: "clone-by-affk.firebaseapp.com",
+  databaseURL: "https://clone-by-affk.firebaseio.com",
+  projectId: "clone-by-affk",
+  storageBucket: "clone-by-affk.appspot.com",
+  messagingSenderId: "905532903769",
+  appId: "1:905532903769:web:c8b6e60a59c75b1e0cbc48",
+  measurementId: "G-ZCB57YPPN2"
+};
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export{db, auth};
